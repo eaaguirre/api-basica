975204c473d97ee8bad6758c984b283a0984d803
eaaguirre <edinson.aguirre@gmail.com>
2025-04-10 20:49:02 -0500

    Segundo Pipeline

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ const PORT = process.env.PORT || 3000;
 app.get('/', (req, res) => res.send('¡Hola Mundo desde Docker!'));
 app.get('/health', (req, res) => res.send({ status: 'ok' }));
 
-app.listen(PORT, () => console.log(`App corriendo en puerto ${PORT}`));
+if (require.main === module) {
+
+    app.listen(PORT, () => console.log(`App corriendo en puerto ${PORT}`));
+
+  }
 
 module.exports = app;
