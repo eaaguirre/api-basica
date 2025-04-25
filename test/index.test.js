fe0d0e6466b6bfc6796d97553340d7853f2be6fc
eaaguirre <edinson.aguirre@gmail.com>
2025-04-24 19:50:17 -0500

    Despliegue 2025

--- test/index.test.js
+++ test/index.test.js
@@ -5,7 +5,7 @@ describe('Pruebas de API', () => {
   it('GET / debe responder con Hola Mundo', async () => {
     const res = await request(app).get('/');
     expect(res.statusCode).toBe(200);
-    expect(res.text).toBe('¡Hola Mundo desde Docker! Edinson Aguirre');
+    expect(res.text).toBe('¡Hola Mundo desde Docker! Edinson Aguirre 2025');
   });
 
   it('GET /health debe responder con estado ok', async () => {
